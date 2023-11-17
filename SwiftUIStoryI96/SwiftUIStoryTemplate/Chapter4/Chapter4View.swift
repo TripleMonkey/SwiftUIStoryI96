d371847e755197ed41c4541929a9a92678482bce
Sabrina Foreman <samsimmons1738@gmail.com>
2023-11-17 14:15:53 -0500

    Bri update

--- SwiftUIStoryI96/SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
+++ SwiftUIStoryI96/SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
@@ -2,10 +2,66 @@ import SwiftUI
 
 struct Chapter4View: View {
     var body: some View {
-        Text("Chapter 4")
+        //.spacer()
+        //.spacer()
+        ScrollView{
+            Text("Hello, friend.")
+            
+            Text("Under the sea lived a crab named Crabby")
+            
+            Text("Crabby was old and very wise with a long journey ahead of him")
+            
+            Text("He needed to figure out how to get from one side of the beach to the other")
+            
+            Image("Crabby")
+            
+            Text("The journey for a human was only five steps while for Crabby it was 10 steps")
+            
+            Text("This was a major issue because Crabby hasn't made a journey like this in two days")
+            
+            Text("How will Crabby get to the other side of the beach with his legs instead of human legs, he thought to himself")
+            
+            Text("After time briefly passed, it was time to take the first step")
+            
+            Text("Crabby took his first step and he could feel the sun beaming on his foot")
+            
+            Text("The sun! It's hot and bright! Screamed Crabby")
+            
+            Text("By the time he reached his third step he ran into a problem, there was cotton candy in the pathway")
+            
+            Text("Crabby didn't want to waste time or make the process longer so, he ate through the cotton candy")
+            
+            Text("After eating through the cotton candy he proceeded on")
+            
+            Image("Crabby2")
+            
+            Text("Crabby was thirsty and felt like turning back after the fifth step until he stumbled upon some water")
+            
+            Text ("Crabby began to drinking the water but, quickly spit it out because it was too salty")
+            
+            Image("Crabby3ng")
+            
+            Text("Crabby was even more thirsty and began to be a little faint")
+           
+            Text("Crabby laid in the sand and began to close his eyes")
+            
+        Image("Crabbyng4")
+           
+            Text("Crabby woke up and realized that he was somehow moved to the other side of the beach")
+            
+            Text("He cheered until it started to rain")
+           
+            Text("Then Crabby woke up again to realize it was all a dream")
+            Image("Crabby")
+            
+            Text("Looking sad and hopeless Crabby turned back and thought to do the journey on another day")
+           
+            Text("The End")
+            
+        }
     }
 }
-
 #Preview {
     Chapter4View()
 }
+
